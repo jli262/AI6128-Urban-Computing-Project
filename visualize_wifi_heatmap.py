0245863f06893fb819b655c6b7cf7a17e74a37de
Joy LI <lijiayi262@gmail.com>
2022-10-09 19:05:11 +0800

    Add KNN model

--- visualize_wifi_heatmap.py
+++ visualize_wifi_heatmap.py
@@ -3,6 +3,6 @@ from src.data import FloorPlan
 
 if __name__ == '__main__':
     data_directory = os.path.join(os.getcwd(), 'data')
-    floorplan = FloorPlan(1, 'B1', data_directory)
+    floorplan = FloorPlan(1, 'F1', data_directory)
     floorplan.plot_wifi_heatmap('dBm')
     floorplan.show()

3b475cdf7a5bb48cfeee06d1b08fde75208b28ca
Lim Yuan Qing <yuanqing87@gmail.com>
2022-11-19 18:02:59 +0800

    updated task 2 and generated nicer outputs

--- project_2/task_2.py
+++ project_2/task_2.py
@@ -4,6 +4,8 @@ logger = logging.getLogger(__name__)
 import os
 from src.data import Trips, RoadNetwork
 import matplotlib.colors as mcolors
+import osmnx
+import folium
 
 if __name__ == '__main__':
     datapath = os.path.join(os.path.join(os.getcwd(), 'data'), 'train_1000.csv')
@@ -24,6 +26,18 @@ if __name__ == '__main__':
         road_network.plot_trip(trip, color='red')
         road_network.save_plot(os.path.join(output_directory, uid))
         road_network.plot_graph()
+        i+=1
+
+    i = 1
+    color_list = ['red',  'orange', 'yellow',  'olive', 'green', 'blue', 'purple', 'cyan',  'pink', 'black']
+    for trip in first_ten:
+        uid = 'trip_{num}.html'.format(num=i)
+        G = road_network.graph
+        route = eval(trip.POLYLINE)
+        route = [(lati, longi) for longi, lati in route]
+        map_ = osmnx.folium.plot_graph_folium(G, color='#D3D3D3', weight = 1, zoom = 14, zoom_control = False)
+        folium.PolyLine(route, color=color_list[i-1], weight=7, opacity=0.8).add_to(map_)
+        map_.save(outfile=os.path.join(output_directory, uid))
         i+=1
 
 
